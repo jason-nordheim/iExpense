f9aa5d45dfac6248a44afd06da33463a7cac3dc4
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-17 13:07:14 -0700

    Created <Profile /> page component

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import { PageTitle } from "../components/common/PageTitle";
+
+export const Profile = () => {
+  return (
+    <>
+      <div className="row">
+        <h1>Profile</h1>
+        <span className="em-divider"></span>
+      </div>
+    </>
+  );
+};
