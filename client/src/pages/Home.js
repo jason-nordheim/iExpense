941512be56310b1b0ef7574e5b0b22faf2fda01a
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-17 13:07:24 -0700

    added <Home /> page component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import { PageTitle } from "../components/common/PageTitle";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="row">
+        <PageTitle value={"Home"} />
+      </div>
+    </>
+  );
+};
