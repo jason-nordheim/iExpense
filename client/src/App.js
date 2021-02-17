03532cbb9bab0a99245f72d6246b7733239fb841
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-17 13:09:52 -0700

    implemented '/' and '/profile' routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
-import { Login } from "./components/Login";
-
+import { NavBar } from "./components/NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 export const App = () => {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <main>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 };
