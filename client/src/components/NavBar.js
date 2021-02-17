3109a3e82ba6696b146441b972e309964efb45c9
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-17 14:40:32 -0700

    updated <Navbar /> component to hide side bar on large screens, only displaying links along the top of the page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as M from "materialize-css/dist/js/materialize";
 
 export const NavBar = () => {
@@ -9,44 +9,66 @@ export const NavBar = () => {
   const handleNavLinkClick = (event) => {
     if (sideNav) sideNav.close();
   };
-  return (
-    <>
-      <ul ref={sideNavRef} id="slide-out" className="sidenav sidenav-fixed">
+
+  const NavLinks = () => {
+    return (
+      <>
         <li>
-          <div className="user-view"></div>
-        </li>
-        <li>
-          <Link
+          <NavLink
             to="/"
             about="Homepage"
             className="waves-effect"
             onClick={handleNavLinkClick}
           >
-            <i className="material-icons">account_balance</i>Expense Me
-          </Link>
+            <i className={`material-icons`}>home</i>
+            <span>Home</span>
+          </NavLink>
         </li>
         <li>
-          <div className="divider"></div>
-        </li>
-        <li>
-          <Link
+          <NavLink
             className="waves-effect"
             to="/profile"
             onClick={handleNavLinkClick}
           >
-            Profile
-          </Link>
+            <i className="material-icons">account_circle</i>
+            <span>Profile</span>
+          </NavLink>
         </li>
+      </>
+    );
+  };
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper purple darken-4">
+          <NavLink
+            to="/"
+            about="Homepage"
+            className="brand-logo center"
+            onClick={handleNavLinkClick}
+          >
+            <i className="material-icons">account_balance</i>
+            Expense Me
+          </NavLink>
+          <div className="right">
+            <a href="#!" data-target="slide-out" className="sidenav-trigger">
+              <i className="material-icons em-menu-icon ">menu</i>
+            </a>
+          </div>
+          <ul className="right hide-on-med-and-down">
+            <NavLinks />
+          </ul>
+        </div>
+      </nav>
+      <ul ref={sideNavRef} id="slide-out" className="sidenav">
+        <li>
+          <div className="container"></div>
+        </li>
+        <li>
+          <div className="divider"></div>
+        </li>
+        <NavLinks />
       </ul>
-      <div className="right">
-        <a
-          href="#!"
-          data-target="slide-out"
-          className="sidenav-trigger btn-floating purple darken-4"
-        >
-          <i className="material-icons em-menu-icon ">menu</i>
-        </a>
-      </div>
     </>
   );
 };
