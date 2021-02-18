a69c5c3740f05df1f17831ce78a14aea442d7e4d
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-18 15:30:23 -0700

    implemented provider for user store

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { NavBar } from "./components/NavBar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
+
+import { Provider } from "react-redux";
+import { userStore } from "./auth/_user.store";
+
 export const App = () => {
   return (
     <BrowserRouter>
-      <main className="purple lighten-4" style={{ height: "100vh" }}>
-        <NavBar />
-        <Switch>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </main>
+      <Provider store={userStore}>
+        <main className="purple lighten-4" style={{ height: "100vh" }}>
+          <NavBar />
+          <Switch>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Provider>
     </BrowserRouter>
   );
 };
