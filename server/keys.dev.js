5b5c711a1460651a8367b57f70b8a25b6cf47e15
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-18 10:20:43 -0700

    updated such that it no longer includes keys fiel

---
+++ server/keys.dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoUri:
+    "mongodb+srv://dbAdmin:XuTG1RMJw56bmUII@cluster0.42qtm.mongodb.net/XuTG1RMJw56bmUII?retryWrites=true&w=majority",
+  tokenSecret: "SDGJsgds2235dgSDKFGYHERY@%",
+};
