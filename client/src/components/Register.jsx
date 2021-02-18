1c0e0394e19bb7faaa6148f53a0d3fa01ee3e30a
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-18 15:29:23 -0700

    implemented 'dispatch' for login and register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,58 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { InputField } from "./common/InputField";
+import { useDispatch } from 'react-redux';
+import { USER_ACTIONS } from "../auth/_user.actions";
 
 export const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginEnabled, setLoginEnabled] = useState(false);
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginEnabled, setLoginEnabled] = useState(false);
 
-  useEffect(() => {
-    if (username && username.length > 5 && password && password.length > 5) {
-      setLoginEnabled(true);
-    } else setLoginEnabled(false);
-  }, [username, password]);
+    useEffect(() => {
+        const hasValidCreds = () => username && username.length > 5 && password && password.length > 5;
+        setLoginEnabled(hasValidCreds());
+    }, [username, password]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h3>Register</h3>
-      <div className="row">
-        <InputField
-          type="text"
-          htmlFor="username"
-          placeholder="Username"
-          value={username}
-          setValue={setUsername}
-          minLength={5}
-          required
-        />
-      </div>
-      <div className="row">
-        <InputField
-          type="password"
-          placeholder="Password"
-          value={password}
-          setValue={setPassword}
-          minLength={5}
-          required
-        />
-      </div>
-      <div className="row">
-        <button
-          className={`waves-effect waves-light btn ${
-            !loginEnabled && "disabled"
-          }`}
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Register
-        </button>
-      </div>
-      <div className="row">
-        Already have an account? Login <a href="#!">Here</a>
-      </div>
-    </form>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loginEnabled && dispatch(USER_ACTIONS.register(username, password));
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <h3>Register</h3>
+            <div className="row">
+                <InputField
+                    type="text"
+                    htmlFor="username"
+                    placeholder="Username"
+                    value={username}
+                    setValue={setUsername}
+                    minLength={5}
+                    required
+                />
+            </div>
+            <div className="row">
+                <InputField
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    setValue={setPassword}
+                    minLength={5}
+                    required
+                />
+            </div>
+            <div className="row">
+                <button
+                    className={`waves-effect waves-light btn ${!loginEnabled && "disabled"
+                        }`}
+                    type="submit"
+                    onClick={handleSubmit}
+                >
+                    Register
+                </button>
+            </div>
+        </form>
+    );
 };
