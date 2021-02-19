253c06e0404aef069d1995134471523c1fad35c5
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-19 13:52:24 -0700

    removed middleware

--- client/src/auth/_user.store.js
+++ client/src/auth/_user.store.js
@@ -1,13 +1,8 @@
 import { createStore } from "redux";
 import { userReducer } from "./_user.reducer";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
 
 /**
  * Create a Redux store holding the state of your app.
  * Its API is { subscribe, dispatch, getState }.
  */
-export let userStore = createStore({
-  reducer: userReducer,
-  middleware: [thunk, logger],
-});
+export let userStore = createStore(userReducer);
