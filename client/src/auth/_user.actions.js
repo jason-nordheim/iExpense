4b41ee1a50b6a950353e42921660b13b29a19136
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-23 12:46:21 -0700

    updated userAction creators to match new reducer function

--- client/src/auth/_user.actions.js
+++ client/src/auth/_user.actions.js
@@ -1,82 +1,63 @@
 import { USER_CONSTANTS } from "./_user.constants";
 import { userService } from "./_userService";
-/**
- * action creators related to users
- */
+
 export const userActions = {
   login,
   logout,
   register,
+  alreadyAuthenticated,
 };
 
-/**
- * 1) dispatches `LOGIN_REQUEST` action via `dispatch(loginRequest({ username }))`
- * 2) calls async task `userService.login(username, password)`
- * 3) dispatches result of `userService.login(username, password)`
- *     -> Success: dispatches a `LOGIN_SUCCESS` with `dispatch(loginSuccess(user));`
- *     -> Failure:  dispatches a `LOGIN_FAILURE` action with `dispatch(loginFailure(error));`
- * @param {Function} dispatch
- * @param {String} username
- * @param {String} password
- */
 function login(dispatch, username, password) {
-  const user = { username, password };
+  loginRequest();
 
-  loginRequest(user);
-
-  userService.login(username, password).then(
+  userService.login({ username, password }).then(
     (token) => loginSuccess(token),
     (error) => loginFailure(error)
   );
 
-  function loginRequest(payload) {
-    console.log("loginRequest", payload);
-    return dispatch({ type: USER_CONSTANTS.LOGIN_REQUEST, payload });
+  function loginRequest() {
+    const action = { type: USER_CONSTANTS.LOGIN_REQUEST };
+    return dispatch(action);
   }
   function loginSuccess(payload) {
-    console.log("loginSuccess", payload);
-    return dispatch({ type: USER_CONSTANTS.LOGIN_SUCCESS, payload });
+    const action = { type: USER_CONSTANTS.LOGIN_SUCCESS, payload };
+    return dispatch(action);
   }
-  function loginFailure(payload) {
-    console.log("loginFailure", payload);
-    return dispatch({ type: USER_CONSTANTS.LOGIN_FAILURE, payload });
+  function loginFailure(error) {
+    const action = { type: USER_CONSTANTS.LOGIN_FAILURE, payload: { error } };
+    return dispatch(action);
   }
 }
 
-/**
- * Removes the token associated with the current user
- */
 function logout(dispatch) {
   userService.logout();
   return dispatch({ type: USER_CONSTANTS.LOGOUT });
 }
 
-/**
- * 1) dispatches `REGISTER_REQUEST` action via `dispatch(registerRequest({ username, password }))`
- * 2) calls async task `userService.register(username, password)`
- * 3) dispatches result of `userService.register(username, password)`
- *     -> Success: dispatches a `REGISTER_SUCCESS` with `dispatch(registerSuccess(user));`
- *     -> Failure:  dispatches a `REGISTER_FAILURE` action with `dispatch(registerFailure(error));`
- * @param {String} username
- * @param {String} password
- */
 function register(dispatch, username, password) {
   registerRequest({ username, password });
+
   userService.register(username, password).then(
     (user) => registerSuccess(user),
     (error) => registerFailure(error)
   );
 
   function registerRequest(user) {
-    console.log("registerRequest", user);
-    return dispatch({ type: USER_CONSTANTS.REGISTER_REQUEST, user });
+    return dispatch({ type: USER_CONSTANTS.REGISTER_REQUEST, payload: user });
   }
   function registerSuccess(user) {
-    console.log("registerSuccess", user);
-    return dispatch({ type: USER_CONSTANTS.REGISTER_SUCCESS, user });
+    return dispatch({ type: USER_CONSTANTS.REGISTER_SUCCESS, payload: user });
   }
   function registerFailure(error) {
-    console.log("registerFailure", error);
-    return dispatch({ type: USER_CONSTANTS.REGISTER_FAILURE, error });
+    return dispatch({ type: USER_CONSTANTS.REGISTER_FAILURE, payload: error });
   }
+}
+
+function alreadyAuthenticated(dispatch, payload) {
+  const action = {
+    type: USER_CONSTANTS.ALREADY_AUTHENTICATED,
+    payload,
+  };
+  return dispatch(action);
 }
