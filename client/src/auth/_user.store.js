0b8741e1ec56cab671570a9f65664fb43c20c6fe
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-23 12:46:46 -0700

    added statement in createStore() function enabling redux dev tools

--- client/src/auth/_user.store.js
+++ client/src/auth/_user.store.js
@@ -5,4 +5,7 @@ import { userReducer } from "./_user.reducer";
  * Create a Redux store holding the state of your app.
  * Its API is { subscribe, dispatch, getState }.
  */
-export let userStore = createStore(userReducer);
+export let userStore = createStore(
+  userReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
