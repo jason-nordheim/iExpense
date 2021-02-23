beb885ebe62669f6da9843d73e3630157ed6dd99
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-23 12:44:34 -0700

    added logic to display currently logged in user

--- client/src/components/MyAccount.jsx
+++ client/src/components/MyAccount.jsx
@@ -1,10 +1,26 @@
-
+import { useState, useEffect } from 'react';
+import { useStore } from 'react-redux';
+import { userActions } from "../auth/_user.actions";
 
 export const MyAccount = () => {
+    const authStore = useStore();
+    const [authState, setAuthState] = useState(authStore.getState());
+
+    useEffect(() => authStore.subscribe(() => setAuthState(authStore.getState())), [authStore]);
+
+    const handleLogout = (event) => {
+        event.preventDefault();
+        userActions.logout(authStore.dispatch);
+    };
+
     return (
         <>
+            <div className="card-action right-align">
+                <a class="waves-effect waves-light btn blue accent-4"
+                    onClick={handleLogout}>Logout</a>
+            </div>
             <div className="card-content">
-                Welcome
+                Welcome {authState.user.username}
             </div>
         </>
     );
